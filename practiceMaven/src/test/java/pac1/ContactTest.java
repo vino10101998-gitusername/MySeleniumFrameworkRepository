449e2025e5d3bc40683a47967e11274edcb8d667
vino10101998-gitusername <vino10101998@gmail.com>
2025-11-23 11:06:35 +0530

    Initial commit

---
+++ practiceMaven/src/test/java/pac1/ContactTest.java
@@ -0,0 +1,21 @@
+package pac1;
+
+import org.testng.annotations.Test;
+
+public class ContactTest {
+
+	@Test
+	public void createContactTest() {
+		System.out.println("Execute create contact test");
+	}
+	
+	@Test
+	public void modifyContactTest() {
+		System.out.println("Execute modify contact test");
+	}
+	
+	@Test
+	public void createContactWithOrgTest() {
+		System.out.println("Execute create contact with org test");
+	}
+}
---
+++ practiceMaven/src/test/java/pac2/OrgTest.java
@@ -0,0 +1,21 @@
+package pac2;
+
+import org.testng.annotations.Test;
+
+public class OrgTest {
+
+	@Test
+	public void createOrgTest() {
+		System.out.println("Execute create org test");
+	}
+	
+	@Test
+	public void modifyOrgTest() {
+		System.out.println("Execute modify org test");
+	}
+	
+	@Test
+	public void createOrgWithEmailTest() {
+		System.out.println("Execute create org with email test");
+	}
+}
